3cf91c18606c61b9db0174dceee41d1122efcc3e
Laider Lucas <laiderlucas@hotmail.com>
2020-01-17 17:17:32 -0400

    construido a pagina web e integrado com o backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,7 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -19,6 +20,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-pqbpe.mongodb.net/w
     useUnifiedTopology: true,
 })
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
