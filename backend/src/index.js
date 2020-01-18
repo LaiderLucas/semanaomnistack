16b6c343cff48b1e441a067dc4171f33752e8caf
Laider Lucas <laiderlucas@hotmail.com>
2020-01-18 14:31:15 -0400

    criado o aplicativo moblie

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,9 +11,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-pqbpe.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -25,6 +30,6 @@ app.use(express.json());
 app.use(routes);
 
 
-app.listen(3333);
+server.listen(3333);
 
 
